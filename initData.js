7de63e4e16f35ce0358a21d00509b460f9acee3a
SrayaAvraham-w <srayaa@elementor.com>
2023-06-12 14:39:49 +0300

    fix delete data

--- initData.js
+++ initData.js
@@ -5,18 +5,17 @@ const config = {
   password: 'admin',
   host: 'localhost',
   port: 5432,
-  database: 'ba-test'
+  database: 'loadTest'
 };
 
 const tableName = 'relation_tuples';
 
-async function dropTable() {
+async function deleteFromTable() {
   const client = new Client(config);
 
   try {
     await client.connect();
     await client.query(`DELETE FROM ${tableName}`);
-    console.log(`Data deleted from table '${tableName}' successfully.`);
   } catch (error) {
     console.error('Error dropping table:', error);
   } finally {
@@ -24,4 +23,4 @@ async function dropTable() {
   }
 }
 
-dropTable();
+deleteFromTable();
